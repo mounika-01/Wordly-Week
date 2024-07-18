91cd2a8e257c52aa22de79c374c96051a7aee949
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-18 12:54:50 +0530

    Update WriterJpaRepository.java

--- src/main/java/com/example/wordlyweek/repository/WriterJpaRepository.java
+++ src/main/java/com/example/wordlyweek/repository/WriterJpaRepository.java
@@ -9,11 +9,11 @@
 // Write your code here
 package com.example.wordlyweek.repository;
 
-import org.springframework.data..jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.wordlyweek.model.Writer;
 
 @Repository
-public interface WriterJpaRepository extends JpaRepository<Writer, Integer>{
-    
+public interface WriterJpaRepository extends JpaRepository<Writer, Integer> {
+
 }
