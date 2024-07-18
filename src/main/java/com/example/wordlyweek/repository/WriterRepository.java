75eb530e9afef99ca5ceddaaf17ed964a61232a2
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-18 12:55:11 +0530

    Update WriterRepository.java

--- src/main/java/com/example/wordlyweek/repository/WriterRepository.java
+++ src/main/java/com/example/wordlyweek/repository/WriterRepository.java
@@ -1,8 +1,9 @@
-package com.example.wordlyWeek.repository;
+package com.example.wordlyweek.repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.example.wordlyWeek.model.*;
+import com.example.wordlyweek.model.Writer;
+import com.example.wordlyweek.model.Magazine;
 
 public interface WriterRepository {
     ArrayList<Writer> getWriters();
